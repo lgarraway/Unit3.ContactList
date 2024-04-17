7ddc053c0e89a4ea6f117d57f1850b1c7592932d
Londell <lodellg@icloud.com>
2024-04-17 12:43:56 -0500

    Set up Table.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import  ContactList  from './components/ContactList.jsx'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-     
+        <ContactList />
       </div>
     </>
   )
